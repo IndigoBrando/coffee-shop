16e40ce36a8ab36259cd3f7bd02bf29e4b77fe13
Christian Malimit <christianmalimit07@gmail.com>
2025-08-17 16:10:09 +0800

    removed 1 import exception e

--- src/main/java/com/example/coffee_shop/controller/CoffeeController.java
+++ src/main/java/com/example/coffee_shop/controller/CoffeeController.java
@@ -8,9 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;              // ✅ add this
+import java.nio.file.Path;              
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
